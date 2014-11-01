d03d463e838267afcf1f6e111b5512c18d104ad1
pjc0247_msi <pjc0247@naver.com>
2014-11-01 21:10:00 +0900

    Random 클래스 인터페이스 추가

---
+++ src/Classes/util/Random.cpp
@@ -0,0 +1,17 @@
+#include "Random.h"
+
+#include <stdlib.h>
+
+void Random::setSeed(
+	unsigned int seed[States]){
+}
+void Random::setIndex(
+	unsigned int index){
+}
+unsigned int Random::getIndex(){
+	return 0;
+}
+
+int Random::rand(){
+	rand();
+}
---
+++ src/Classes/util/Random.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Random{
+public:
+	static const int States = 16;
+
+public:
+	void setSeed(
+		unsigned int seed[States]);
+	void setIndex(
+		unsigned int index);
+	unsigned int getIndex();
+
+	int rand();
+};
