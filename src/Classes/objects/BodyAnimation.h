7b8925b5f69376e0ede878401f9134ac17bf12d6
Min-soo Park <rinirihiriro@gmail.com>
2014-11-02 20:31:41 +0900

    BodyAnimation 추가

---
+++ src/Classes/objects/BodyAnimation.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "cocos2d.h"
+
+#include "PartedBody.h"
+
+class BodyAnimation : public cocos2d::Ref
+{
+public:
+	static const int AnimationDelay = 1.f;
+
+public:
+	static BodyAnimation* create(
+		const std::string& filename, const std::vector<int>& partsId, const int frame);
+	bool init(
+		const std::string& filename, const std::vector<int>& partsId, const int frame);
+
+	cocos2d::Animation* getPartAnimation(const int id);
+
+private:
+	BodyAnimation();
+
+private:
+	cocos2d::Animation* partAnimation[PartedBody::Parts];
+};
