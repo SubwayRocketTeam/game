1a2f8ef52250e2703ee97afea0c6cf15f2225a55
Min-soo Park <rinirihiriro@gmail.com>
2014-11-02 22:03:43 +0900

    Resource file name 변경
    Parted Body로 통합한 관계로 이름 수정

--- src/Classes/common/resource.cpp
+++ src/Classes/common/resource.cpp
@@ -3,12 +3,12 @@
 
 namespace R{
 	_RESD(PlayerBody, "character1");
-	_RESD(EnemyBody, "bang.png");
+	_RESD(EnemyBody, "bang");
 
 	_RESD(StageFloor, "Floor.png");
 	_RESD(StageJson, "stage.json");
 
-	_RESD(EnemySpawner, "bang.png");
+	_RESD(EnemySpawner, "bang");
 
 	_RESD(SkillList, "skills.json");
 }
