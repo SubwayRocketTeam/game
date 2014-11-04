8e81cb63191492717ce7355e96f16810d21b2922
Park June Chul <pjc0247@naver.com>
2014-11-04 23:37:57 +0900

    AttrName 추가

--- src/Classes/skill/AttributeName.h
+++ src/Classes/skill/AttributeName.h
@@ -8,4 +8,9 @@ namespace Attr{
 	const std::string mp = "mp";
 	const std::string attack = "attack";
 	const std::string defence = "defence";
+	const std::string mpRegen = "mp_regen";
+	const std::string attackSpeed = "attack_speed";
+	const std::string criticalRate = "critical_rate";
+	const std::string accuracyRate = "accracy_rate";
+	const std::string evasionRate = "evasion_rate";
 };
