7d81259824d59445b49226c80aada2c200b283f6
clogic <choiseungil29@gmail.com>
2014-11-04 22:19:54 +0900

    중복되는 Attributes클래스 중 하나 제거.

--- src/Classes/objects/Unit.h
+++ src/Classes/objects/Unit.h
@@ -1,10 +1,8 @@
 #pragma once
 
 #include "cocos2d.h"
-
 #include "skill/Attribute.h"
 
-struct Attributes;
 class PartedBody;
 
 class Unit : public cocos2d::Sprite{
@@ -55,6 +53,5 @@ protected:
 
 	cocos2d::LabelTTF *dbgAngle;
 
-	Attributes* attr;
 	std::map<std::string,Attribute> attrs;
 };
