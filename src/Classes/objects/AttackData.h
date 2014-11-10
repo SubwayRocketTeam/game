f1dd7288e1a597bf98eeddf8d1c0d7b5413ceca6
Min-soo Park <rinirihiriro@gmail.com>
2014-11-10 21:45:37 +0900

    AttackData 정보 추가

--- src/Classes/objects/AttackData.h
+++ src/Classes/objects/AttackData.h
@@ -1,5 +1,22 @@
 #pragma once
 
-struct AttackData{
+#include "cocos2d.h"
 
+class Unit;
+
+enum AttackType {
+	PAN,
+	LINE,
+};
+
+struct AttackData{
+	Unit* user;
+	AttackType type;
+	float damage;
+
+	cocos2d::Vec2 startPostion;
+	cocos2d::Vec2 targetPostion;
+
+	float radius;
+	float halfAngle;
 };
