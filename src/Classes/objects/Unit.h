f3a09e9b13ca6189d0f01f7366cbc50c71245ac5
Park June Chul <pjc0247@naver.com>
2014-11-19 17:44:37 +0900

    _ATTR 매크로 수정

--- src/Classes/objects/Unit.h
+++ src/Classes/objects/Unit.h
@@ -9,9 +9,9 @@
 #include "objects/AttackData.h"
 #include "objects/Ally.h"
 
-#define _ATTR(name) (attrs[Attr::##name].get())
-#define _ATTR_VALUE(name) (attrs[Attr::##name].getValue())
-#define _ATTR_MAX(name) (attrs[Attr::##name].getMax())
+#define _ATTR(name) getAttribute(Attr::##name).get()
+#define _ATTR_VALUE(name) getAttribute(Attr::##name).getValue()
+#define _ATTR_MAX(name) getAttribute(Attr::##name).getMax()
 #define _INIT_ATTR(name, value) (attrs[Attr::##name].set(value))
 
 class Gauge;
