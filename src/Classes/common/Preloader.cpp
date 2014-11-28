140626b49251fcb26e63ed83c1fdb1430a0c7faa
pjc0247_msi <pjc0247@naver.com>
2014-11-28 23:44:02 +0900

    Preloader 작성

--- src/Classes/common/Preloader.cpp
+++ src/Classes/common/Preloader.cpp
@@ -1,10 +1,130 @@
 ﻿#include "pch.h"
 #include "Preloader.h"
 
+#include "audio/include/SimpleAudioEngine.h"
+
+#include <vector>
+
 using namespace std;
 using namespace cocos2d;
+using namespace CocosDenshion;
+
+#define _BRANCHED_CALL(prefix ,type, name) switch(type){ \
+	case ResourceType::BGM: \
+		prefix##BGM(name); \
+		break; \
+	case ResourceType::SoundEffect: \
+		prefix##SoundEffect(name); \
+		break; \
+	case ResourceType::Plist: \
+		prefix##Plist(name); \
+		break; \
+	};
+
+struct Preloader::Transaction{
+	vector<string> bgms;
+	vector<string> effects;
+	vector<string> plists;
+};
+struct Preloader::ResourceData{
+	ResourceType type;
+	string name;
+	int ref;
+};
+
+static Preloader *instance = nullptr;
 
 Preloader::Preloader(){
 }
 Preloader::~Preloader(){
+}
+
+Preloader *Preloader::create(){
+	auto p = new Preloader();
+
+	if(p) return p;
+	CC_SAFE_DELETE(p);
+	return nullptr;
+}
+Preloader *Preloader::getInstance(){
+	return instance;
+}
+
+bool Preloader::push(
+	const string &name){
+
+	return true;
+}
+void Preloader::pop(
+	const string &name){
+}
+
+void Preloader::loadBGM(
+	const string &name){
+
+	/* DO NOTHING */
+}
+void Preloader::loadSoundEffect(
+	const string &name){
+
+	SimpleAudioEngine::getInstance()
+		->preloadEffect(name.c_str());
+}
+void Preloader::loadPlist(
+	const string &name){
+
+	SpriteFrameCache::getInstance()
+		->addSpriteFramesWithFile(name);
+}
+void Preloader::unloadBGM(
+	const string &name){
+
+	/* DO NOTHING */
+}
+void Preloader::unloadSoundEffect(
+	const string &name){
+
+	SimpleAudioEngine::getInstance()
+		->unloadEffect(name.c_str());
+}
+void Preloader::unloadPlist(
+	const string &name){
+
+	SpriteFrameCache::getInstance()
+		->removeSpriteFramesFromFile(name);
+}
+
+void Preloader::load(
+	ResourceType type,
+	const string &name){
+
+	auto &pair = resources.find(name);
+
+	/* LOAD */
+	if(pair == resources.end()){
+		_BRANCHED_CALL(load, type, name);
+
+		auto &data = resources[name];
+		data.name = name;
+		data.type = type;
+		data.ref = 1;
+	}
+	else
+		pair->second.ref ++;
+}
+void Preloader::unload(
+	ResourceType type,
+	const string &name){
+
+	CC_ASSERT(resources.find(name) != resources.end());
+
+	auto &data = resources[name];
+	data.ref --;
+
+	/* RELEASE */
+	if(data.ref == 0){
+		_BRANCHED_CALL(load, type, name);
+
+		resources.erase(name);
+	}
 }
--- src/Classes/common/Preloader.h
+++ src/Classes/common/Preloader.h
@@ -1,15 +1,55 @@
 ﻿#pragma once
 
-class Preloader{
+#include "cocos2d.h"
+
+#include <string>
+#include <stack>
+#include <map>
+
+class Preloader : cocos2d::Ref{
 private:
-	
+	struct Transaction;
+	struct ResourceData;
+
+	enum ResourceType{
+		BGM,
+		SoundEffect,
+		Plist
+	};
 
 public:
+	static Preloader *create();
+	static Preloader *getInstance();
+	
+	bool push(
+		const std::string &name);
+	void pop(
+		const std::string &name);
 
 protected:
 	Preloader();
 	virtual ~Preloader();
 
-private:
+	virtual void loadBGM(
+		const std::string &name);
+	virtual void loadSoundEffect(
+		const std::string &name);
+	virtual void loadPlist(
+		const std::string &name);
+	virtual void unloadBGM(
+		const std::string &name);
+	virtual void unloadSoundEffect(
+		const std::string &name);
+	virtual void unloadPlist(
+		const std::string &name);
 
+	void load(
+		ResourceType type,
+		const std::string &name);
+	void unload(
+		ResourceType type,
+		const std::string &name);
+
+private:
+	std::map<std::string,ResourceData> resources;
 };
