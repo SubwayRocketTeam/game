83562b3b71b9fbab58243524c2c166c3f81a9ccb
pjc0247_msi <pjc0247@naver.com>
2014-12-04 23:46:29 +0900

    fix indent

--- tests/packet_protocol.rb
+++ tests/packet_protocol.rb
@@ -2,15 +2,15 @@ module PacketProtocol
   def receive_data data
     (@buf||= '') << data
 
-	while @buf.size >= 4
-	  if @buf.size >= (size=@buf.unpack('I').first)
-	    receive_packet @buf.slice! 0, size
-	  end
-	end
+    while @buf.size >= 4
+      if @buf.size >= (size=@buf.unpack('I').first)
+        receive_packet @buf.slice! 0, size
+      end
+    end
   end
 
   def send_packet packet
-	send_data packet.serialize
+	  send_data packet.serialize
   end
   def receive_packet packet
     # override me
