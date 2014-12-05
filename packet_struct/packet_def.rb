1cdf5c44bc15b451a706fc78bdcd3f04566d56d0
Park June Chul <pjc0247@naver.com>
2014-12-05 14:50:14 +0900

    패킷 스키마 작성

--- packet_struct/packet_def.rb
+++ packet_struct/packet_def.rb
@@ -1,3 +1,4 @@
+# LOGIN
 class LoginRequest < Packet
   required
   string "id", 32
@@ -7,4 +8,63 @@ class LoginResponse < Packet
   required
   int "result"
   string "nickname", 32
+end
+
+# ROOM
+class EnterRoom < Packet
+  required
+end
+class LeaveRoom < Packet
+  required
+end
+class EnterNoti < Packet
+  required
+  int "client_id"
+end
+class LeaveNoti < Packet
+  required
+  int "client_id"
+end
+
+# UNIT
+class SpawnUnit < Packet
+  required
+  int "id"
+  int "unit_type"
+  float "x"
+  float "y"
+end
+
+# MOVE
+class MoveStart < Packet
+  required
+  float "direction_x"
+  float "direction_y"
+end
+class MoveEnd < Packet
+  required
+end
+class MoveStartNoti < Packet
+  required
+  int "id"
+  float "start_x"
+  float "start_y"
+  float "velocity_x"
+  float "velocity_y"
+end
+class MoveEndNoti < Packet
+  required
+  int "id"
+  float "end_x"
+  float "end_y"
+end
+
+# CHAT
+class ChatMessage < Packet
+  required
+  string "msg", 64
+end
+class ChatNoti < Packet
+  required
+  string "msg", 64
 end
