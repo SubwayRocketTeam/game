8eee0c6ae46728bb64e8fe9ca98027e29f0112e4
Park June Chul <pjc0247@naver.com>
2014-12-09 03:17:36 +0900

    Update tests.rb

--- tests/tests.rb
+++ tests/tests.rb
@@ -3,26 +3,26 @@ load '../packet_structure/packet_def.rb'
 
 class LoginTest < RocketTestUnit
   def query
-  	p = LoginRequest.new
+    p = LoginRequest.new
     p.id = "pjc0247"
     p.pw = "asdf1234"
     return p
   end
   def should
-  	p = LoginResponse.new
-    p.nickname = "Anaz"
-    p.result = 12
+    p = LoginResponse.new
+    p.nickname = SKIP
+    p.result = 1
     return p
   end
 end
 
 class EnterRoomTest < RocketTestUnit
   def query
-  	p = EnterRoom.new
+    p = EnterRoom.new
     return p
   end
   def should
-  	p = EnterNoti.new
+    p = EnterNoti.new
     p.client_id = SKIP
     return p
   end
