4d20535a0bdd6073f8a84d5ad8a178f9867ee015
Park June Chul <pjc0247@naver.com>
2014-12-09 03:16:32 +0900

    Update tests.rb

--- tests/tests.rb
+++ tests/tests.rb
@@ -1,4 +1,33 @@
-load 'rocket_test_unit.rb'
+load 'test_unit.rb'
+load '../packet_structure/packet_def.rb'
+
+class LoginTest < RocketTestUnit
+  def query
+  	p = LoginRequest.new
+    p.id = "pjc0247"
+    p.pw = "asdf1234"
+    return p
+  end
+  def should
+  	p = LoginResponse.new
+    p.nickname = "Anaz"
+    p.result = 12
+    return p
+  end
+end
+
+class EnterRoomTest < RocketTestUnit
+  def query
+  	p = EnterRoom.new
+    return p
+  end
+  def should
+  	p = EnterNoti.new
+    p.client_id = SKIP
+    return p
+  end
+end
+
 
 RocketTestPool.execute \
-  []
+  [LoginTest, EnterRoomTest]
