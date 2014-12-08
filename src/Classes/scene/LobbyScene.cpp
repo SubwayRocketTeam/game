402aaa54d80f5d43b1a07f18797819634b3b8dbc
Park June Chul <pjc0247@naver.com>
2014-12-08 20:26:05 +0900

    로비 씬 프로토타입_

---
+++ src/Classes/scene/LobbyScene.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "LobbyScene.h"
+#include "GameScene.h"
+
+#include "audio/include/SimpleAudioEngine.h"
+
+using namespace std;
+using namespace cocos2d;
+using namespace cocostudio;
+using namespace CocosDenshion;
+
+LobbyScene::LobbyScene(){
+}
+LobbyScene::~LobbyScene(){
+}
+
+Scene* LobbyScene::scene(){
+	auto scene = Scene::create();
+	LobbyScene *layer = LobbyScene::create();
+	scene->addChild(layer);
+	return scene;
+}
+
+bool LobbyScene::init(){
+	if(!Layer::init())
+		return false;
+	
+	auto reader = GUIReader::getInstance();
+	auto layout = reader->widgetFromJsonFile("lobby_scene.json");
+
+	addChild(layout);
+
+	return true;
+}
---
+++ src/Classes/scene/LobbyScene.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "cocos2d.h"
+
+#include "editor-support/cocostudio/CCSGUIReader.h"
+
+class LobbyScene : public cocos2d::Layer{
+public:
+	CREATE_FUNC(LobbyScene);
+
+    static cocos2d::Scene* scene();
+
+protected:
+	LobbyScene();
+	virtual ~LobbyScene();
+
+	virtual bool init();
+};
