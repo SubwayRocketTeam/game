358220dc723a1ba777b1287a71b76fa0249246f4
Park June Chul <pjc0247@naver.com>
2014-12-10 01:07:30 +0900

    Ref.h 추가

---
+++ src/Server/Ref.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <atomic>
+
+class Ref{
+	typedef std::atomic<int> counter_t;
+
+	Ref(){};
+	virtual ~Ref(){};
+
+	void retain(){
+		refCount++;
+	}
+	void release(){
+		refCount--;
+
+		if(refCount == 0)
+			delete this;
+	}
+
+private:
+	counter_t refCount;
+};
