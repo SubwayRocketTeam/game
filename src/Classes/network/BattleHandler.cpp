1e70773265a02a6c98e2a9e499d57cf0f31ba75b
Park June Chul <pjc0247@naver.com>
2014-12-12 21:08:28 +0900

    자기가 스킬 쓴 거 무시

--- src/Classes/network/BattleHandler.cpp
+++ src/Classes/network/BattleHandler.cpp
@@ -1,6 +1,7 @@
 ﻿#include "pch.h"
 #include "Network.h"
 
+#include "objects/Player.h"
 #include "objects/Unit.h"
 #include "objects/Bullet.h"
 #include "objects/Stage.h"
@@ -11,6 +12,11 @@ void Network::handleUseSkillNoti(
 	UseSkillNoti *pkt){
 
 	auto unit = Unit::getInstanceByID(pkt->id);
+	auto player = Player::getInstance();
+
+	if(pkt->id == player->getID())
+		return;
+
 	unit->useSkill(
 		pkt->skill_id,
 		Vec2(pkt->x, pkt->y));
