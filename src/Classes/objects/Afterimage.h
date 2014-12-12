d6439b41f153572ce89c76d629aa017c01072c21
Park June Chul <pjc0247@naver.com>
2014-12-12 20:34:12 +0900

    Afterimage::init protected로

--- src/Classes/objects/Afterimage.h
+++ src/Classes/objects/Afterimage.h
@@ -9,21 +9,22 @@ public:
 	static Afterimage *create(
 		cocos2d::Node *world,
 		cocos2d::Sprite *target, float duration);
-	virtual bool init(
-		cocos2d::Node *world,
-		cocos2d::Sprite *target, float duration);
 
 protected:
 	Afterimage();
 	virtual ~Afterimage();
 
+	virtual bool init(
+		cocos2d::Node *world,
+		cocos2d::Sprite *target, float duration);
+
+	virtual void update(
+		float dt);
+
 	void addAfterimage(
 		cocos2d::Sprite *target,
 		cocos2d::Vec2 pos, float angle, float scale,
 		int opacity);
-
-	virtual void update(
-		float dt);
 
 private:
 	cocos2d::Node *world;
