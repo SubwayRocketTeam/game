c542d81238b2189ac97be850ce08e335723d1cc9
Park June Chul <pjc0247@naver.com>
2014-12-19 22:54:10 +0900

    필요없는거

---
+++ src/Server/DBContext.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <string>
+
+class DBContext{
+public:
+	bool connect(
+		const std::string &conn);
+	void close();
+	
+	bool execute(
+		const std::string &query);
+
+	bool push(
+		int value);
+	bool push(
+		float value);
+	bool push(
+		const std::string &value);
+
+	int popAsInt();
+	float popAsFloat();
+	std::string popAsString();
+
+private:
+	DBContext();
+	virtual ~DBContext();
+
+
+};
